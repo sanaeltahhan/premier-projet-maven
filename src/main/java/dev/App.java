e11ee6492baf26761a4ae32cc9411eb4afaaea12
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-10 01:38:37 +0200

    Etape 7 : Integration d'une librairie tierce

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,4 +1,7 @@
 package main.java.dev;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
 
 /**
  * Hello world!
@@ -6,8 +9,13 @@ package main.java.dev;
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+
+        // using default font standard.flf, obtained from maven artifact
+        String asciiArt1 = FigletFont.convertOneLine("Hello World");
+        System.out.println(asciiArt1);
     }
 }
